9e93b51779e174a046c8e7eb061252b369e36ad5
gonejack <igonejack@gmail.com>
2022-06-09 00:05:43 +0800

    support seperated drives

--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"github.com/antonfisher/nested-logrus-formatter"
-	"github.com/sirupsen/logrus"
+	"log"
 )
-
-func init() {
-	logrus.SetFormatter(&formatter.Formatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-		//NoColors:        true,
-		HideKeys:    true,
-		CallerFirst: true,
-	})
-}
 
 func main() {
 	err := new(transcode).run()
 	if err != nil {
-		logrus.Fatal(err)
+		log.Fatal(err)
 	}
 }
--- transcode.go
+++ transcode.go
@@ -4,23 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/alecthomas/kong"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/htmlindex"
 	"golang.org/x/text/transform"
 )
 
 type options struct {
-	SourceEncoding string `short:"s" name:"source-encoding" default:"gbk" help:"Set source encoding."`
-	TargetEncoding string `short:"t" name:"target-encoding" default:"utf8" help:"Set target encoding."`
-	Overwrite      bool   `short:"r" name:"overwrite" help:"Overwrite source file."`
-	Verbose        bool   `short:"v" help:"Verbose printing."`
-	About          bool   `help:"Show about."`
-
-	File []string `arg:"" optional:""`
+	SourceEncoding string   `short:"s" name:"source-encoding" default:"gbk" help:"Set source encoding."`
+	TargetEncoding string   `short:"t" name:"target-encoding" default:"utf8" help:"Set target encoding."`
+	Overwrite      bool     `short:"r" name:"overwrite" help:"Overwrite source file."`
+	About          bool     `help:"Show about."`
+	File           []string `arg:"" optional:""`
 }
 type transcode struct {
 	options
@@ -50,103 +46,47 @@ func (c *transcode) run() (err error) {
 	}
 
 	if len(c.File) == 0 {
-		c.File = []string{"-"}
-		c.Verbose = false
+		c.File = append(c.File, "-")
 	}
 
-	if c.Verbose {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-
-	for _, file := range c.File {
-		logrus.Debugf("process %s start", file)
-		err := c.process(file)
+	for _, f := range c.File {
+		err = c.process(f)
 		if err != nil {
-			logrus.WithError(err).Errorf("process %s error", file)
+			return
 		}
-		logrus.Debugf("process %s end", file)
 	}
 
 	return
 }
-
 func (c *transcode) process(file string) (err error) {
-	source, target, writeDisk := os.Stdin, os.Stdout, file != "-"
+	source, target := os.Stdin, os.Stdout
 
-	if writeDisk {
-		var abs string
-		// resolve source path
-		{
-			log := logrus.WithField("step", "RESOLVE")
-			log.Debugf("resolve %s", file)
-			abs, err = filepath.Abs(file)
+	if file != "-" {
+		source, err = os.OpenFile(file, os.O_RDWR, 0755)
+		if err != nil {
+			return
+		}
+		defer source.Close()
+		if c.Overwrite {
+			target, err = os.CreateTemp(os.TempDir(), "")
 			if err != nil {
-				return fmt.Errorf("cannot parse file path %s", file)
-			}
-			log.Debugf("resolve %s done: %s", file, abs)
-		}
-		// open source file
-		{
-			log := logrus.WithField("step", "OPEN")
-			log.Debugf("open %s", abs)
-			source, err = os.Open(abs)
-			if err == nil {
-				defer source.Close()
-			} else {
 				return
 			}
-			log.Debugf("open %s ok", abs)
-		}
-		// create temp file
-		{
-			log := logrus.WithField("step", "OPEN")
-			log.Debugf("create temp file")
-			target, err = os.CreateTemp("", fmt.Sprintf("transcode_%s.", filepath.Base(source.Name())))
-			if err == nil {
-				defer target.Close()
-			} else {
-				return
-			}
-			log.Debugf("create temp file %s ok", target.Name())
+			defer target.Close()
+			defer func() {
+				if err == nil {
+					source.Seek(0, io.SeekStart)
+					target.Seek(0, io.SeekStart)
+					_, err = io.Copy(source, target)
+				}
+			}()
 		}
 	}
 
-	// transfer source => target/temp file
-	log := logrus.WithField("step", "TRANSFER")
-	log.Debugf("transfer %s => %s", source.Name(), target.Name())
 	_, err = io.Copy(
 		transform.NewWriter(target, c.target.NewEncoder()),
 		transform.NewReader(source, c.source.NewDecoder()),
 	)
-	if err != nil {
-		return fmt.Errorf("translate %s => %s failed: %s", source.Name(), target.Name(), err)
-	}
-	log.Debugf("transfer %s => %s done", source.Name(), target.Name())
-
-	// renaming target file
-	if writeDisk {
-		output := source.Name() + ".transcode" + filepath.Ext(source.Name())
-
-		if c.Overwrite {
-			log := logrus.WithField("step", "REMOVE")
-			log.Debugf("remove %s", source.Name())
-			err = os.Remove(source.Name())
-			if err != nil {
-				return fmt.Errorf("remove %s failed: %w", source.Name(), err)
-			}
-			log.Debugf("remove %s done", source.Name())
-			output = source.Name()
-		}
-
-		log := logrus.WithField("step", "RENAME")
-		log.Debugf("rename %s => %s", target.Name(), output)
-		err = os.Rename(target.Name(), output)
-		if err != nil {
-			return fmt.Errorf("rename %s => %s failed: %w", target.Name(), output, err)
-		}
-		log.Debugf("rename %s => %s done", target.Name(), output)
-		logrus.Infof("save into %s", output)
-	}
 
 	return
 }
